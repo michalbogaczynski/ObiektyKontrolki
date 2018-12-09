222912b04fcb4117977c8dd46ae3bf99984bff30
michalbogaczynski <bogaczynski.m@gmail.com>
2018-12-09 23:26:33 +0100

    Odbijające kontrolki

---
+++ KontrolkiObiekty/LabelBouncer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KontrolkiObiekty
+{
+    class LabelBouncer
+    {
+        public Label MyLabel;
+
+        public bool GoinForward = true;
+
+        public void Move()
+        {
+            if (MyLabel != null)
+            {
+                if (GoinForward == true)
+                {
+                    MyLabel.Left += 5;
+                    if (MyLabel.Left >= MyLabel.Parent.Width - MyLabel.Width)
+                    {
+                        GoinForward = false;
+                    }
+                }
+            }
+            else
+            {
+                MyLabel.Left -= 5;
+                if (MyLabel.Left <= 0)
+                {
+                    GoinForward = true;
+                }
+            }
+        }
+    }
+}
